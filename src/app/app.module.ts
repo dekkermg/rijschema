49f9de26be7d4e74ad616e651d4e7e7cf09a5fac
dekkermg <dekker.mg@outlook.com>
2017-01-03 15:00:25 +0100

    eerste aanpassingen
    d

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './home/welcome.component'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', redirectTo: 'welcome', pathMatch: 'full' }      
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    WelcomeComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/ritten/rit.ts
@@ -0,0 +1,9 @@
+export interface IRit {
+        ritId: 1,
+        ritNaam: string,
+        ritCode: string,
+        ritDatum: string,
+        omschrijving: string,
+        afstand: number,
+        prijsPerKilometer: number
+}
